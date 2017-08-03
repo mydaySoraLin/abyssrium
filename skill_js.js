bb52bcedae0f1ff24b240987c429cd0cf53ca4a4
mydaySoraLin <sora_lin@myday.com.tw>
2017-08-03 15:25:16 +0800

    Update skill_js.js

--- skill_js.js
+++ skill_js.js
@@ -227,7 +227,7 @@ function skill_search()
 	{
 		bg_vol = 1;
 	}
-// alert(sm_cal + " " + m_cal + " " + bg_cal + " " + sm_vol + " " + m_vol + " " + bg_vol);
+	// alert(sm_cal + " " + m_cal + " " + bg_cal + " " + sm_vol + " " + m_vol + " " + bg_vol);
 	// 倍率 = 沙城 * 火山口 * 神秘的貝殼
 	magnification = sm_cal * m_cal * bg_cal * sm_vol * m_vol * bg_vol * myst_shell;
 
@@ -309,60 +309,4 @@ function skill_search()
 
 	$("#final").show();
 	$("#result").html("五分鐘可產出最大量<br/>珊瑚石產量約： " + rock_value + rock_lev_eg + "<br/>珊瑚產量約： " + coral_value + coral_lev_eng);
-}
-
-function up_grade_eng(lev_eng)
-{
-	// 切割字串
-	if(lev_eng.length == 1)
-	{
-		first_eng = "";
-		sec_eng = lev_eng.charCodeAt(0);
-	}
-	if(lev_eng.length == 2)
-	{
-		first_eng = lev_eng.charCodeAt(0);
-		sec_eng = lev_eng.charCodeAt(1);
-	}
-
-	//字母為z或是Z
-	if(sec_eng == 90 || sec_eng == 122)
-	{
-		if(first_eng == '')
-		{
-			if(sec_eng == 90)
-			{
-				first_eng = 65;
-				sec_eng = 65;
-			}
-			if(sec_eng == 122)
-			{
-				first_eng = 97;
-				sec_eng = 97;
-			}
-		}
-		else
-		{
-			first_eng++;
-			if(sec_eng == 90) sec_eng = 65;
-			if(sec_eng == 122) sec_eng = 97;
-		}
-		
-	}
-	else
-	{
-		sec_eng++;
-	}
-
-	return String.fromCharCode(first_eng) + String.fromCharCode(sec_eng);
-}
-
-//只能輸入數字+小數點
-function ValidateNumber(e,pnumber)
-{
-	if (!/^\d+[.]?\d*$/.test(pnumber))
-	{
-		e.value = /^\d+[.]?\d*/.exec(e.value);
-	}
-	return false;
 }
